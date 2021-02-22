5b1146c92adf9328d535ddd205edae207a48aac3
薛猷騰 <jon111rock@gmail.com>
2021-02-22 18:45:39 +0800

    新增 Product列表自動更新

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <!-- ProductType navbar -->
-        <section class="product-nav col-md-2">
+        <section class="product-nav col-md-2" data-aos="fade-up">
           <div class="row justify-content-center mt-2">
             <nav class="nav flex-column ">
               <div v-for="(item, key) in productTypes" :key="key">
@@ -20,61 +20,16 @@
           </div>
         </section>
         <!-- product list -->
-        <section class="product-list col-md-10">
+        <section class="product-list col-md-10" data-aos="fade-up">
           <div class="row">
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/嫩香滷大雞腿.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">嫩香滷大雞腿</h5>
-              <small>NT$199</small>
-            </div>
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/泰美味打拋豬.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">泰美味打拋豬</h5>
-              <small>NT$199</small>
-            </div>
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/清蒸蔥油雞腿.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">清蒸蔥油雞腿</h5>
-              <small>NT$199</small>
-            </div>
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/秘醬老滷排骨.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">秘醬老滷排骨</h5>
-              <small>NT$199</small>
-            </div>
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/紅燒糖醋里肌.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">紅燒糖醋里肌</h5>
-              <small>NT$199</small>
-            </div>
-            <div class="col-md-4 product">
-              <img
-                src="../assets/img/青蔥資料/經典京醬肉絲.jpg"
-                alt=""
-                class="product-img"
-              />
-              <h5 class="mt-1">經典京醬肉絲</h5>
-              <small>199$NT</small>
+            <div
+              class="col-md-4 product"
+              v-for="(item, key) in products"
+              :key="key"
+            >
+              <img :src="item.photoUrl" alt="" class="product-img" />
+              <h5 class="mt-1">{{ item.name }}</h5>
+              <small>NT$ {{ item.price }} </small>
             </div>
           </div>
         </section>
@@ -101,24 +56,32 @@ export default {
       var vm = this;
       this.products = [];
       this.axios
-        .get("https://localhost:5001/api/products")
+        .get(`${process.env.VUE_APP_APIPATH}/products`)
         .then((res) => {
           res.data.forEach((i) => {
             if (i.productTypeId == vm.currentType.productTypeId) {
-              vm.products.push(i);
+              if (i.state == 1) {
+                vm.products.push(i);
+              }
             }
           });
-          console.log(this.products);
+          // console.log(this.products);
         })
         .catch((err) => {
           console.log("取得產品失敗", err);
         });
     },
     getProductTypes() {
+      var vm = this;
+      this.productTypes = [];
       this.axios
-        .get("https://localhost:5001/api/producttypes")
+        .get(`${process.env.VUE_APP_APIPATH}/producttypes`)
         .then((res) => {
-          this.productTypes = res.data;
+          res.data.forEach((i) => {
+            if (i.state == 1) {
+              vm.productTypes.push(i);
+            }
+          });
           this.currentType = this.productTypes[0];
         })
         .catch((err) => {
